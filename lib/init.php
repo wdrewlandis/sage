e81e9ed34a3f02e0fd5e55c5c6cc5ca32d88bbf5
Paul <peiche@gmail.com>
2015-09-09 08:42:07 -0500

    Created mini and mega footer widget areas

--- lib/init.php
+++ lib/init.php
@@ -55,12 +55,21 @@ function widgets_init() {
   ]);
 
   register_sidebar([
-    'name'          => __('Footer', 'sage'),
-    'id'            => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget'  => '</section>',
-    'before_title'  => '<h3>',
-    'after_title'   => '</h3>'
+    'name'          => __('Mini Footer', 'sage'),
+    'id'            => 'sidebar-mini-footer',
+    'before_widget' => '<div class="mdl-mini-footer__left-section">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<div class="mdl-logo">',
+    'after_title'   => '</div>'
+  ]);
+
+  register_sidebar([
+    'name'          => __('Mega Footer', 'sage'),
+    'id'            => 'sidebar-mega-footer',
+    'before_title'  => '<input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked><h1 class="mdl-mega-footer__heading">',
+    'after_title'   => '</h1><div class="mdl-mega-footer__link-list">',
+    'before_widget' => '<section class="mdl-mega-footer__drop-down-section">',
+    'after_widget'  => '</div></section>',
   ]);
 }
 add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="mdl-mini-footer" role="contentinfo">
+  <?php dynamic_sidebar('sidebar-mega-footer'); ?>
+  <?php dynamic_sidebar('sidebar-mini-footer'); ?>
 </footer>
