751725246be102ad595289e58c573a4581c0f79f
Paul <peiche@gmail.com>
2015-09-09 13:04:09 -0500

    Added checks for footers in use

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,13 @@
+<?php if ( is_active_sidebar( 'sidebar-mega-footer' ) ) { ?>
+<footer class="mdl-mega-footer" role="contentinfo">
+  <div class="mdl-mega-footer__middle-section">
+    <?php dynamic_sidebar('sidebar-mega-footer'); ?>
+  </div>
+</footer>
+<?php } ?>
+
+<?php if ( is_active_sidebar( 'sidebar-mini-footer' ) ) { ?>
 <footer class="mdl-mini-footer" role="contentinfo">
-  <?php dynamic_sidebar('sidebar-mega-footer'); ?>
   <?php dynamic_sidebar('sidebar-mini-footer'); ?>
 </footer>
+<?php } ?>
